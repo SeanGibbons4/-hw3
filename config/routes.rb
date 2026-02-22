47db7c666d9596ec82795bccddfdd42176b9334d
SeanGibbons4 <sean.gibbons@kellogg.northwestern.edu>
2026-02-22 22:29:41 +0000

    Homework #3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get "/travel_log", { controller: "places", action: "index" }
+  get "/places/new", { controller: "places", action: "new" }
+  post "/places", { controller: "places", action: "create" }
+  get "/places/:id", { controller: "places", action: "show" }
+  get "/places/:place_id/entries/new", { controller: "entries", action: "new" }
+  post "/entries", { controller: "entries", action: "create" }
 end
